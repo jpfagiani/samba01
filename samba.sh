0dd103d6ccb234364fba660670b9681e469bdea8
jpfagiani <61874180+jpfagiani@users.noreply.github.com>
2024-08-22 14:53:53 -0300

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -3,11 +3,11 @@
 apt update 
 hostnamectl set-hostname ad.cdpni.sap
 apt install openssh-server nfs-client samba krb5-config winbind krb5-user smbclient -y
+mv /etc/samba/smb.conf smb.conf.old
 cp -v /root/samba/etc/* /etc/
 	systemctl restart networking sshd
 	chattr +i /etc/resolv.conf
- mv /etc/samba/smb.conf smb.conf.old
- samba-tool domain provision --use-rfc2307 --interactive
+samba-tool domain provision --use-rfc2307 --interactive
 	systemctl stop smbd nmbd winbind
 	systemctl disable smbd nmbd winbind
 	systemctl unmask samba-ad-dc
