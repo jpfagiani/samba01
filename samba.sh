cf4e3838a89cfcce7ed8163f199810af84399695
jpfagiani <61874180+jpfagiani@users.noreply.github.com>
2024-08-24 12:00:23 -0300

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -4,7 +4,7 @@ apt update
 hostnamectl set-hostname ad.cdpni.sap
 apt install openssh-server nfs-client samba krb5-config winbind krb5-user smbclient -y
 mv /etc/samba/smb.conf smb.conf.old
-cp -v /root/samba/etc/* /etc/
+cp -rv /root/samba/etc/* /etc/
 	systemctl restart networking sshd
 	chattr +i /etc/resolv.conf
 samba-tool domain provision --use-rfc2307 --interactive
